965455e68e928b2fd1161694d30ef9764c448714
4d696e6820427569 <mdbui@email.arizona.edu>
2021-08-15 20:33:00 -0500

    Added 19.3 EPI

---
+++ EPI/ch19_graphs/19_3_enclosed_regions.cc
@@ -0,0 +1,13 @@
+/**
+ * EPI Chapter 19 Graphs
+ *
+ * 19.3 Compute enclosed regions.
+ *
+ * Let A be a 2D array whose entries are either W or B. Write a program that
+ * takes A and replaces all Ws that cannot reach the boundary with a B.
+ *
+ * Hint: It's easier to compute the complement of the desired result.
+ */
+
+/**
+ * Idea: 
