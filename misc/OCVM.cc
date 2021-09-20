eca396009790c612a88d5f24caba63c3989bb03a
4d696e6820427569 <mdbui@email.arizona.edu>
2021-09-19 20:44:22 -0500

    Fixed some integer types, code more concise

--- misc/OCVM.cc
+++ misc/OCVM.cc
@@ -17,7 +17,7 @@ using std::unordered_map;
 using std::string;
 using std::pair;
 
-const unordered_map<string, pair<int, bool>> kOpCodes{
+const unordered_map<string, pair<uint8_t, bool>> kOpCodes{
     {"hlt", {1, false}},
     {"psh", {2, true}},
     {"pop", {3, false}},
@@ -35,15 +35,15 @@ class CoolAssembler
 public:
     CoolAssembler() = delete;
 
-    CoolAssembler(unsigned long memorySizeInBytes, unsigned long printBeginOffset,
-            unsigned short bytesPerElement, unsigned long numElementsToPrint,
-            unsigned long numElementsPerLine)
+    CoolAssembler(uint64_t memorySizeInBytes, uint64_t printBeginOffset, 
+            uint8_t bytesPerElement, uint64_t numElementsToPrint,
+            uint64_t numElementsPerLine)
         : memory_size_(memorySizeInBytes)
         , begin_offset_(printBeginOffset)
         , bytes_per_element_(bytesPerElement)
         , num_elements_(numElementsToPrint)
         , num_elements_per_line_(numElementsPerLine)
-        , mem_(new unsigned char[memory_size_])
+        , mem_(new uint8_t[memory_size_])
     {
 
     }
@@ -60,15 +60,15 @@ public:
     void PrintMemory()
     {
         // Potential overflow here.
-        unsigned long end_addr = begin_offset_ + num_elements_ * bytes_per_element_;
-        unsigned long count_element = 0;
+        uint64_t end_addr = begin_offset_ + num_elements_ * bytes_per_element_;
+        uint64_t count_element = 0;
 
         // Also want to do bound checking here. Invalid input? end_addr might go pass
         // mem_ total size.
         //
         // Bound checking
         end_addr = end_addr < memory_size_ ? end_addr : memory_size_;
-        for (unsigned long start_addr = begin_offset_; start_addr < end_addr; 
+        for (uint64_t start_addr = begin_offset_; start_addr < end_addr; 
                 start_addr += bytes_per_element_) {
             if (count_element == num_elements_per_line_) {
                 printf("\n");
@@ -76,7 +76,7 @@ public:
             }
 
             // Print each element.
-            for (unsigned short i = 0; i < bytes_per_element_; i++) {
+            for (uint8_t i = 0; i < bytes_per_element_; i++) {
                 printf("%02hhx", mem_[start_addr + i]);
             }
 
@@ -89,18 +89,17 @@ public:
     {
         // Get ASM instructions.
         string asm_instruction;
-        unsigned long start_addr = 0;
+        uint64_t start_addr = 0;
         while (std::getline(std::cin, asm_instruction)) {
 
             // Read the instruction into memory.
             string op_code = asm_instruction.substr(0, 3);
             if (kOpCodes.find(op_code) != kOpCodes.end()) {
-                unsigned long op_code_encoded = (kOpCodes.at(op_code).first) << 0x2;
+                uint64_t op_code_encoded = (kOpCodes.at(op_code).first) << 0x2;
 
-                unsigned short width_char = asm_instruction[3] - '0';
-                unsigned long width = 0;
+                uint8_t width_char = asm_instruction[3] - '0';
+                uint64_t width = 0;
 
-                
                 switch (width_char) {
                     case 2:
                         width = 0x1;
@@ -128,9 +127,9 @@ public:
                 // according to the width.
                 if (kOpCodes.at(op_code).second) {
                     string param = asm_instruction.substr(5);
-                    long param_int = std::stol(param);
+                    int64_t param_int = std::stol(param);
 
-                    for (unsigned i = width_char; i > 0; i--) {
+                    for (int i = width_char; i > 0; i--) {
                         mem_[start_addr + i] = param_int & 0xFF;
                         param_int >>= 0x8;
                     }
@@ -145,30 +144,32 @@ public:
 
 private:
     // Memory size in bytes.
-    unsigned long memory_size_;
+    uint64_t memory_size_;
 
     // Offset address to print out.
-    unsigned long begin_offset_;
+    uint64_t begin_offset_;
 
     // Number of bytes per element.
-    unsigned short bytes_per_element_;
+    uint8_t bytes_per_element_;
 
     // Number of elements.
-    unsigned long num_elements_;
+    uint64_t num_elements_;
 
     // Number of elements per line.
-    unsigned long num_elements_per_line_;
-    unsigned char* mem_;
+    uint64_t num_elements_per_line_;
+    
+    // Memory segment.
+    uint8_t* mem_;
 };
 
 
-void PrintMemory(unsigned char mem[], unsigned long memorySizeInBytes,
-        unsigned long printBeginOffset, unsigned short bytesPerElement,
-        unsigned long numElementsToPrint, unsigned long numElementsPerLine)
+void PrintMemory(uint8_t mem[], uint64_t memorySizeInBytes,
+        uint64_t printBeginOffset, uint8_t bytesPerElement,
+        uint64_t numElementsToPrint, uint64_t numElementsPerLine)
 {
-    unsigned long end_addr = printBeginOffset + numElementsToPrint * bytesPerElement;
+    uint64_t end_addr = printBeginOffset + numElementsToPrint * bytesPerElement;
     end_addr = end_addr < memorySizeInBytes ? end_addr : memorySizeInBytes;
-    unsigned long countElement = 0;
+    uint64_t countElement = 0;
 
     for (size_t start_addr = printBeginOffset; start_addr < end_addr; start_addr += bytesPerElement) {
         if (countElement == numElementsPerLine) {
@@ -187,19 +188,19 @@ void PrintMemory(unsigned char mem[], unsigned long memorySizeInBytes,
     printf("\n");
 }
 
-void GetASMInstructions(unsigned char mem[])
+void GetASMInstructions(uint8_t mem[])
 {
     // Get ASM instructions.
     string asm_instruction;
-    unsigned long start_addr = 0;
+    uint64_t start_addr = 0;
     while (std::getline(std::cin, asm_instruction)) {
         // Read the instruction into memory.
         string op_code = asm_instruction.substr(0, 3);
         if (kOpCodes.find(op_code) != kOpCodes.end()) {
-            unsigned long op_code_encoded = (kOpCodes.at(op_code).first) << 0x2;
+            uint64_t op_code_encoded = (kOpCodes.at(op_code).first) << 0x2;
 
-            unsigned short width_char = asm_instruction[3] - '0';
-            unsigned long width = 0;
+            uint8_t width_char = asm_instruction[3] - '0';
+            uint64_t width = 0;
             switch (width_char) {
                 case 2:
                     width = 0x1;
@@ -211,7 +212,7 @@ void GetASMInstructions(unsigned char mem[])
                     width = 0x3;
             }
 
-            unsigned char width_test = 0;
+            uint8_t width_test = 0;
             while (width_char >>= 1) width_test++;
             assert(width_test == width);
 
@@ -220,13 +221,13 @@ void GetASMInstructions(unsigned char mem[])
             // Write opcode and the width to the first byte.
             mem[start_addr] = op_code_encoded;
 
-            // CHeck if there's a param. If there's a param, write it to memory
+            // Check if there's a param. If there's a param, write it to memory
             // according to the width.
             if (kOpCodes.at(op_code).second) {
                 string param = asm_instruction.substr(5);
                 long param_int = std::stol(param);
 
-                for (unsigned i = width_char; i > 0; i--) {
+                for (int i = width_char; i > 0; i--) {
                     mem[start_addr + i] = param_int & 0xFF;
                     param_int >>= 0x8;
                 }
@@ -241,24 +242,23 @@ void GetASMInstructions(unsigned char mem[])
 
 int main()
 {
-    unsigned long memorySizeInBytes = 0;
-    unsigned long printBeginOffset = 0;
-    unsigned short bytesPerElement = 1;
-    unsigned long numElementsToPrint = 0;
-    unsigned long numElementsPerLine = 0;
+    uint64_t memorySizeInBytes = 0;
+    uint64_t printBeginOffset = 0;
+    uint64_t numElementsToPrint = 0;
+    uint64_t numElementsPerLine = 0;
+    uint8_t bytesPerElement = 1;
 
     // Get memory config.
-    scanf("%lu %lu %hu %lu %lu", &memorySizeInBytes, &printBeginOffset,
+    scanf("%lu %lu %hhu %lu %lu", &memorySizeInBytes, &printBeginOffset,
             &bytesPerElement, &numElementsToPrint, &numElementsPerLine);
 
-    unsigned char mem[memorySizeInBytes];
+    uint8_t mem[memorySizeInBytes];
     memset(mem, 0, memorySizeInBytes);
     PrintMemory(mem, memorySizeInBytes, printBeginOffset, bytesPerElement, 
             numElementsToPrint, numElementsPerLine);
     GetASMInstructions(mem);
     PrintMemory(mem, memorySizeInBytes, printBeginOffset, bytesPerElement, 
             numElementsToPrint, numElementsPerLine);
-
 
     return 0;
 }
