aa3f7f8fcfbc1e0af0c1a16102c05331dde118aa
4d696e6820427569 <mdbui@email.arizona.edu>
2021-09-26 12:05:20 -0500

    Working on error handling

--- misc/OCVM.cc
+++ misc/OCVM.cc
@@ -35,6 +35,7 @@
 //#define _DEBUG
 
 #include <unordered_map>
+#include <limits>
 #include <sstream>
 #include <iostream>
 #include <iomanip>
@@ -59,6 +60,13 @@ const unordered_map<string, pair<uint8_t, bool>> kOpCodes{
     {"mlt", {8, true}},
     {"jmp", {9, true}},
     {"jmf", {10, true}}
+};
+
+struct IOStream
+{
+    std::istream& vm_in;
+    std::ostream& vm_out;
+    std::ostream& vm_err;
 };
 
 struct AssemblerConfiguration
@@ -104,7 +112,6 @@ struct AssemblerConfiguration
         return *this;
     }
 
-
     // Memory size in bytes.
     uint64_t memory_size_;
 
@@ -129,9 +136,7 @@ public:
     CoolAssembler(const AssemblerConfiguration& config)
         : asm_config_(config)
         , mem_(new uint8_t[asm_config_.memory_size_])
-    {
-
-    }
+    {}
 
     CoolAssembler(const CoolAssembler&) = delete;
     CoolAssembler& operator=(const CoolAssembler&) = delete;
@@ -175,7 +180,10 @@ public:
             delete[] mem_;
     }
 
-    void PrintMemory(std::ostream& outp_stream) const
+    /**
+     * Dump the memory in hex of in mem_ to the output device.
+     */
+    void PrintMemory(const IOStream& io_stream) const
     {
         // Potential overflow here. How to deal with it?
         uint64_t end_addr = asm_config_.begin_offset_ + 
@@ -192,29 +200,32 @@ public:
         for (uint64_t start_addr = asm_config_.begin_offset_; start_addr < end_addr; 
                 start_addr += asm_config_.bytes_per_element_) {
             if (count_element == asm_config_.num_elements_per_line_) {
-                outp_stream << "\n";
+                io_stream.vm_out << "\n";
                 count_element = 0;
             }
 
             // Print each element.
             for (uint8_t i = 0; i < asm_config_.bytes_per_element_; i++) {
-                outp_stream << std::setfill('0') << std::setw(2);
-                outp_stream << std::hex << (0xFF & mem_[start_addr + i]);
+                io_stream.vm_out << std::setfill('0') << std::setw(2);
+                io_stream.vm_out << std::hex << (0xFF & mem_[start_addr + i]);
                 //printf("%02hhx", mem_[start_addr + i]);
             }
 
             count_element++;
-            outp_stream << " ";
+            io_stream.vm_out << " ";
         }
-        outp_stream << "\n";
+        io_stream.vm_out << "\n";
     }
 
-    void GetInstructions(std::istream& inp_stream) const
+    /**
+     * Load the ASM instructions and covert it to binary format.
+     */
+    void GetInstructions(const IOStream& io_stream) const
     {
         // Get ASM instructions.
         string asm_instruction;
         uint64_t start_addr = 0;
-        while (std::getline(inp_stream, asm_instruction)) {
+        while (std::getline(io_stream.vm_in, asm_instruction)) {
 
             // Read the instruction into memory.
             // Need a better way to do this, what happens if the opcode's length is no longer 3?
@@ -268,6 +279,14 @@ public:
         }
     }
 
+    /**
+     * Run the program loaded in mem_
+     */
+    void Run() const
+    {
+
+    }
+
 private:
     // Virtual machine configuration.
     AssemblerConfiguration asm_config_;
@@ -276,35 +295,122 @@ private:
     uint8_t* mem_;
 };
 
+uint64_t GetUInt(const IOStream& io_stream, uint64_t upper_limit)
+{
+    uint64_t result = 0;
+    while (true) {
+        io_stream.vm_in >> result;
+
+        // Check for failed extraction.
+        if (io_stream.vm_in.fail()) {
+            io_stream.vm_in.clear();
+            io_stream.vm_in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            io_stream.vm_in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            return result;
+        }
+    }
+}
+
+uint64_t GetBytesPerElement(const IOStream& io_stream)
+{
+    uint64_t result = 1;
+    while (true) {
+        io_stream.vm_in >> result;
+
+        switch (result) {
+            case 1:
+            case 2:
+            case 4:
+            case 8:
+                return result;
+            default:
+                io_stream.vm_err << "Invalid bytes per element. Either 1, 2, 4, and 8.\n";
+        }
+    }
+}
+
 /**
  * Note to myself: Test cases might overflow operands or results.
- * Initial assumption: Th input line will have 5 numeric arguments on it. We can no longer make the assumption
+ * Initial assumption: The input line will have 5 numeric arguments on it. We can no longer make the assumption
  * that the input is formatted correctly and valid.
+ * 
+ * What kind of input would break the program?
  */
-int ReadConfiguration(std::istream& stdin, AssemblerConfiguration& config)
+int ReadConfiguration(const IOStream& io_stream, AssemblerConfiguration& config)
 {
-    stdin >> config.memory_size_;
+    bool valid_mem_size = false;
+    bool valid_offset = false;
+    bool valid_bytes_per_element = true;
+    bool valid_num_elements = false;
+    bool valid_num_elements_per_line = false;
 
-    stdin >> config.begin_offset_;
+    while (!(valid_mem_size && valid_offset && 
+            valid_bytes_per_element && valid_num_elements &&
+            valid_num_elements_per_line)) {
 
-    // 1, 2, 4, 8 bytes only.
-    stdin >> config.bytes_per_element_;
+        // What could possibly happen here?
+        // 1. User input a too large/too small number.
+        // 2. User input a negative number.
+        // 3. User input a valid number but the OS does not permit such allocation size
+        // for a single buffer.
+        // 4. User input some non-integer characters.
+        io_stream.vm_in >> config.memory_size_;
+        valid_mem_size = true;
 
-    stdin >> config.num_elements_;
+        // What could possibly happen here?
+        // 1. User input a too large / too small number.
+        // 2. User input a negative number.
+        // 3. User input a number which is outside of the range of [0, memory_size_]
+        // 4. User input some non-integer characters.
+        io_stream.vm_in >> config.begin_offset_;
+        valid_offset = true;
 
-    stdin >> config.num_elements_per_line_;
+        // 1, 2, 4, 8 bytes only.
+        // User input anything rather than 1, 2, 4, or 8. 
+        io_stream.vm_in >> config.bytes_per_element_;
+        switch (config.bytes_per_element_) {
+            case 1: break;
+            case 2: break;
+            case 4: break;
+            case 8: break;
+            default:
+                io_stream.vm_err << "Number of bytes per elements must be either 1, 2, 4, or 8.\n";
+                valid_bytes_per_element = false;
+                break;
+        }
 
+        if (!valid_bytes_per_element) {
+            io_stream.vm_in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            io_stream.vm_in.clear();
+            valid_bytes_per_element = true;
+            continue;
+        }
+
+        // 
+        io_stream.vm_in >> config.num_elements_;
+        valid_num_elements = true;
+
+        io_stream.vm_in >> config.num_elements_per_line_;
+        valid_num_elements_per_line = true;
+
+        // Overall:
+        // begin_offset_ + num_elements * bytes_per_element_ < memory_size_;
+        // If not, prompt the user to input again.
+    } 
+    
     #ifdef _DEBUG
     std::cout << config.memory_size_ << " " << config.begin_offset_
         << " " << config.bytes_per_element_ << " " << config.num_elements_
         << " " << config.num_elements_per_line_ << "\n";
     #endif
-    
     return 0;
 }
 
 int main()
 {
+    IOStream io_stream {std::cin, std::cout, std::cerr};
+
     AssemblerConfiguration new_vm_config;
  
     // Get memory config.
@@ -315,17 +421,12 @@ int main()
                 &new_vm_config.num_elements_per_line_);
     */
 
-    #ifdef _DEBUG
-    std::cout << new_vm_config.memory_size_ << " " << new_vm_config.begin_offset_
-        << " " << new_vm_config.bytes_per_element_ << " " << new_vm_config.num_elements_
-        << " " << new_vm_config.num_elements_per_line_ << "\n";
-    #endif
+    ReadConfiguration(io_stream, new_vm_config);
 
-    ReadConfiguration(std::cin, new_vm_config);
     CoolAssembler coolVM1(new_vm_config);
-    coolVM1.PrintMemory(std::cout);
-    coolVM1.GetInstructions(std::cin);
-    coolVM1.PrintMemory(std::cout);
+    coolVM1.PrintMemory(io_stream);
+    coolVM1.GetInstructions(io_stream);
+    coolVM1.PrintMemory(io_stream);
 
     return 0;
 }
