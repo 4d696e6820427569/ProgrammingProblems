21e5a85ec405ccfe9129571aec5b161a0e004521
4d696e6820427569 <mdbui@email.arizona.edu>
2021-10-17 10:09:22 -0500

    Updated more misc problems

---
+++ misc/vg_1.cc
@@ -0,0 +1,28 @@
+/**
+ * VG 1
+ *
+ * Maximum Occurring Character
+ *
+ * Given a string, return the character that appears the maximum number of
+ * times in the string. The string will contain only ASCII characters, from
+ * the ranges ('a'-'z', 'A'-'Z', '0'-'9'), and the case matters. If there is a
+ * tie in the maximum number of times a character appears in the string, return
+ * the character that appears first in the string.
+ *
+ * Example
+ * text = abbbaacc
+ *
+ * Both 'a' and 'b' occur 3 times in text. Since 'a' occurs earlier, a is the
+ * answer.
+ *
+ * Function Description
+ * Complete the function maximumOccurringCharacter
+ *
+ * string text: the string to be operated upon
+ *
+ * returns char: the most occurring character that appears first in the string.
+ *
+ * Constraints
+ * 10 <= | text | <= 10^4
+ * All characters are alphanumeric, in the ranges ('a' - 'z', 'A' - 'Z', '0' - '9')
+ */
