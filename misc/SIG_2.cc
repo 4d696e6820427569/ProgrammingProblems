518faac03ca96860538939e479b7d8295b4e1a7a
4d696e6820427569 <mdbui@email.arizona.edu>
2021-10-20 14:58:49 -0500

    Updated more problem

---
+++ misc/SIG_2.cc
@@ -0,0 +1,11 @@
+/**
+ * SIG 2
+ * Implmeent a simplified version of a crypto broke rplatform. Initially, there
+ * is an array of users, with each user depositing only cash - deposits[i] is
+ * the amount of cash the (i + 1) th user has deposited.
+ *
+ * Assume that for now the platform contains just a single stock and all
+ * operations are done with this stock only. All operations are supposed to be
+ * in the same currency, and currency exchange is not supported for now.
+ *
+ * The platform shoud lsupport the following 3t ypes of operations
