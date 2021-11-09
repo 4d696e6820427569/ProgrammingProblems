dfb4020e780e1263c5bd065077afaefbbfd73fae
4d696e6820427569 <mdbui@email.arizona.edu>
2021-11-08 19:48:57 -0600

    Update closest points problem

--- misc/closest_points.cc
+++ misc/closest_points.cc
@@ -38,12 +38,53 @@ struct Point
     float y;
 };
 
+struct CmpX
+{
+    bool operator()(const Point& p1, const Point& p2)
+    {
+        return p1.x < p2.x || (p1.x == p2.x && p1.y < p2.y);
+    }
+};
+
+struct CmpY
+{
+    bool operator()(const Point& p1, const Point& p2)
+    {
+        return p2.y < p2.y;
+    }
+};
+
 float EuclideanDistance(const Point& p1, const Point& p2)
 {
-    return sqrtf( 
-                (p1.x - p2.x) * (p1.x - p2.x) +
-                (p1.y - p2.y) * (p1.y - p2.y)
-            );
+    return sqrtf((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+}
+
+float GetMinPoints(const Point& p1, const Point& p2, float& min_dist, std::vector<Point>& result)
+{
+    float d = EuclideanDistance(p1, p2);
+    if (d < min_dist) {
+        min_dist = d;
+        if (result.empty()) {
+            result.emplace_back(p1);
+            result.emplace_back(p2);
+        } else {
+            result[0] = p1;
+            result[1] = p2;
+        }
+    }
+    return min_dist;
+}
+
+float ClosestPointsBF(std::vector<Point>& points, int l, int r, float& min_d, std::vector<Point>& result)
+{
+    Point p1, p2;
+    for (int i = l; i < r; i++) {
+        for (int j = i + 1; j < r; j++) {
+            min_d = GetMinPoints(points[i], points[j], min_d, result);
+        }
+    }
+    std::sort(begin(points) + l, begin(points) + r, CmpY());
+    return min_d;
 }
 
 void PrintPoints(const std::vector<Point>& points)
@@ -53,29 +94,49 @@ void PrintPoints(const std::vector<Point>& points)
     }
 }
 
-void ClosestPoints(std::vector<Point>& points, std::vector<Point>& result)
-{
-    std::sort(std::begin(points), std::end(points));
-}
 
-void ClosestPointsBF(std::vector<Point>& points, std::vector<Point>& result)
+float ClosestHelper(std::vector<Point>& points, int l, int r, float& min_dist, std::vector<Point>& result, std::vector<Point>& tmp)
 {
-    float min_d = std::numeric_limits<float>::max();
-    Point p1, p2;
-    for (size_t i = 0; i < points.size(); i++) {
-        for (size_t j = i + 1; j < points.size(); j++) {
-            float cur_dist = EuclideanDistance(points[i], points[j]);
-            if (cur_dist < min_d) {
-                min_d = cur_dist;
-                p1 = points[i];
-                p2 = points[j];
-            }
-        }
+    float min_d = 0;
+    if (r - l <= 3) {
+        return ClosestPointsBF(points, l, r, min_dist, result);
     }
 
-    result.emplace_back(p1);
-    result.emplace_back(p2);
+    int m = l + ((r - l) >> 1);
+    int mid_x = points[m].x;
+    float ld = ClosestHelper(points, l, m, min_dist, result, tmp);
+    float rd = ClosestHelper(points, m, r, min_dist, result, tmp);
+
+    std::merge(begin(points) + l, begin(points) + m, begin(points) + m, begin(points) + r, begin(tmp), CmpY());
+    std::copy(begin(tmp), begin(tmp) + r - l, begin(points) + l);
+
+    int temp_sz = 0;
+    for (int i = l; i < r; i++) {
+        if (fabs(points[i].x - mid_x) < min_dist) {
+            for (int j = temp_sz - 1; j >= 0 && (points[i].y - tmp[j].y) < min_dist; j--) {
+                min_d = GetMinPoints(points[i], tmp[j], min_d, result);
+            }
+            tmp[temp_sz++] = points[i];
+        }
+    }
+    return min_d;
 }
+
+bool FloatEquals(float f1, float f2)
+{
+    return (std::fabs(f1 - f2) <= std::numeric_limits<float>::epsilon() * std::fmax(std::fabs(f1), std::fabs(f2)));
+}
+
+float ClosestPoints(std::vector<Point>& points, std::vector<Point>& result)
+{
+    std::vector<Point> tmp;
+    tmp.resize(points.size());
+
+    std::sort(begin(points), end(points), CmpX());
+    float min_dist = std::numeric_limits<float>::max();
+    return ClosestHelper(points, 0, points.size(), min_dist, result, tmp);
+}
+
 
 int main()
 {
@@ -88,14 +149,21 @@ int main()
     std::vector<Point> closestPointsBF_result;
     std::vector<Point> closestPointsDiv_result;
 
-    ClosestPoints(points, closestPointsDiv_result);
-    ClosestPointsBF(points, closestPointsBF_result);
+    float ddaq = 0;
+    float dbf = 0;
 
+    ClosestPointsBF(points, 0, points.size(), dbf, closestPointsBF_result);
+    ddaq = ClosestPoints(points, closestPointsDiv_result);
+
+    /*
     assert((closestPointsBF_result == closestPointsDiv_result) || [&]() {
         std::reverse(std::begin(closestPointsBF_result), std::end(closestPointsBF_result));
         return closestPointsBF_result == closestPointsDiv_result;
     }());
+    */
 
+    //assert(FloatEquals(ddaq, dbf));
+    printf("%f\n", ddaq);
     PrintPoints(closestPointsDiv_result);
     return 0;
 }
